1a091fe590f4a5d2bd9d9597273e994a7e67c688
Phap Duong Dieu <d2phap@gmail.com>
2021-06-06 19:35:53 +0800

    add checkRectanglesIntersection

--- src/modules/helpers.ts
+++ src/modules/helpers.ts
@@ -1,2 +1,16 @@
 
+export interface IRect {
+  left: number;
+  right: number;
+  top: number;
+  bottom: number;
+}
+
 export const pause = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
+
+export const checkRectanglesIntersection = (rectangle1: IRect, rectangle2: IRect) => !(
+  rectangle2.left > rectangle1.right
+  || rectangle2.right < rectangle1.left
+  || rectangle2.top > rectangle1.bottom
+  || rectangle2.bottom < rectangle1.top
+);
